dace35d7983d490b85a2ff428f3d580197b34fb6
Zenderg <danil54658798@gmail.com>
2026-02-03 23:32:36 +0300

    clear query params on search by url

--- src/tobaccos/dto/find-tobacco-by-url.dto.ts
+++ src/tobaccos/dto/find-tobacco-by-url.dto.ts
@@ -1,8 +1,14 @@
 import { IsString, IsUrl, Matches } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class FindTobaccoByUrlDto {
   @IsUrl()
   @IsString()
+  @Transform(({ value }) => {
+    // Strip query parameters and hash from URL
+    const url = new URL(value);
+    return `${url.origin}${url.pathname}`;
+  })
   @Matches(/^https:\/\/htreviews\.org\/tobaccos\/[^/]+\/[^/]+\/[^/]+$/, {
     message:
       'URL must match format: https://htreviews.org/tobaccos/{brand}/{line}/{tobacco}',
